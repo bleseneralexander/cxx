dc180c09dd36a80220ee67d066c1eec9af2c0f2c
bleseneralexander <113368267+bleseneralexander@users.noreply.github.com>
2023-06-05 15:32:18 -0500

    Create clone.hpp
    setting other useful int main functions available in namespace transform

---
+++ test/src/transform/clone.hpp
@@ -0,0 +1,15 @@
+include:'Int32'
+include:'Clone.cpp'
+
+#implement *Transform
+
+namespace transform main()
+{
+      type main()
+      {
+      }
+// aMovie 
+// CLASS()
+//{ filming TRUE}
+// 
+}
